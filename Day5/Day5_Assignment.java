ee79a0a513915848581172f3db8aac239abf1fb4
sivachandran16 <69729035+sivachandran16@users.noreply.github.com>
2020-08-20 12:47:18 +0530

    Nest4Code Java Batch1 Day5 Assignment

---
+++ Day5/Day5_Assignment.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Day5_Assignment {
+	public static void main(String[] args) {
+		// Day 5 Assignment
+		Scanner userin = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int inp_num = userin.nextInt();
+		int i=0, j=0;
+	for ( i = 0; i < inp_num; i++) {
+
+            for ( j = 0; j <= i ; j++) {
+                String star = "*";
+                System.out.print(star );
+            }
+            System.out.print("\n");
+
+        }
+		
+		for (  i = 0; i <inp_num; i++) {
+
+            for ( j = i; j <inp_num-1 ; j++) {
+                String star = "*";
+                System.out.print(star );
+            }
+            System.out.print("\n");
+
+        }
+		
+    }
+	
+
+}
